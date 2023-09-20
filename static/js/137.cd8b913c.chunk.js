d8c8a454a88cceba48f625fdcf3c7cf2fa575aa0
AnastasiiaDudnik <110532797+AnastasiiaDudnik@users.noreply.github.com>
2023-09-20 14:26:34 +0000

    Deploying to gh-pages from @ AnastasiiaDudnik/shop-test@14d2a316f51ec956e2790afa8ebf6771747b9586 🚀

---
+++ static/js/137.cd8b913c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[137],{137:function(e,r,t){t.r(r);var n=t(790),a=t(184);r.default=function(){var e=(0,n.j)(),r=e.cart,t=e.error;return(0,a.jsxs)(a.Fragment,{children:[t&&(0,a.jsx)("h1",{children:t.message}),r?(0,a.jsx)("ul",{children:r.map((function(e){var r=e._id,t=e.name;return(0,a.jsx)("li",{children:t},r)}))}):(0,a.jsx)("p",{children:"Your cart is empty."})]})}}}]);
+//# sourceMappingURL=137.cd8b913c.chunk.js.map
