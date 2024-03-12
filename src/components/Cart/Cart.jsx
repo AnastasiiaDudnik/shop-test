b21d184e31133560106297b4a451101f2926248d
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2024-03-12 17:42:46 +0200

    cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { getCart } from 'services/cartAPI';
+import { DeleteFromCartBtn } from 'components/DeleteFromCart/DeleteFromCartBtn';
+// import { useCart } from 'services/cartContext';
+
+export const UserCart = () => {
+  const [cart, setCart] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getCart()
+      .then(({ data }) => {
+        setCart(data);
+      })
+      .catch(error => setError(error));
+  }, []);
+
+  // const { cart, error } = useCart();
+  return (
+    <>
+      {error && <h1>{error.message}</h1>}
+      {cart ? (
+        <ul>
+          {cart.map(({ _id, name }) => (
+            <li key={_id}>
+              <p>{name}</p>
+              <DeleteFromCartBtn id={_id} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
+    </>
+  );
+};
